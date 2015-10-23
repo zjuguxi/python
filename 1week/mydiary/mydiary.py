aeb4558afc564c83108a99d8dfd978c61c5093a3
zjuguxi <zjuguxi@gmail.com>
2015-10-23 18:00:22 +0800

    add read function

--- 1week/mydiary/mydiary.py
+++ 1week/mydiary/mydiary.py
@@ -7,6 +7,7 @@ script, diary = argv
 # 打开diary文档
 target = open(diary, 'w')
 # 在line1:后面输入内容
+
 line1 = raw_input("line1:\n")
 # 写入diary
 target.write(line1)
@@ -14,3 +15,8 @@ target.write(line1)
 print "Done!"
 # 关闭并保存文件
 target.close()
+
+target1 = open(diary, "r")
+print target1
+
+target1.close()
