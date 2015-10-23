048991ac56152cb2e1faac620a8a674fe7c75ec2
zjuguxi <zjuguxi@gmail.com>
2015-10-23 17:30:36 +0800

    1w mydiary

---
+++ 1week/mydiary/mydiary.py
@@ -0,0 +1,16 @@
+# coding:utf-8
+# mydiary
+
+from sys import argv
+# 参数
+script, diary = argv
+# 打开diary文档
+target = open(diary, 'w')
+# 在line1:后面输入内容
+line1 = raw_input("line1:\n")
+# 写入diary
+target.write(line1)
+# 打印Done表示任务完成
+print "Done!"
+# 关闭并保存文件
+target.close()
